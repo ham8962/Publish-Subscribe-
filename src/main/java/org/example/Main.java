b4e41733a5430c66a734a211c626a1ed8298c9bb
ham8962 <zzing199842@gmial.com>
2023-07-16 20:58:11 +0900

    Main Class

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Publisher pub = new Publisher("pub1");
+        User user = new User("sub1");
+
+        Thread publisher1 = new Thread(pub);
+        Thread user1 = new Thread(user);
+
+
+        publisher1.start();
+        user1.start();
+
+        publisher1.join();
+        user1.join();
+
+
+
+        System.out.println("FINISH");
+    }
+}
