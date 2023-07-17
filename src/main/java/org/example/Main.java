9a1fafbf20c949b38e0ab0ca5e8de6c4c4465086
ham8962 <zzing199842@gmial.com>
2023-07-17 20:17:42 +0900

    Main Class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        FlagController flagController = new FlagController();
 
-        Publisher pub = new Publisher("pub1");
-        User user = new User("sub1");
+        Publisher pub = new Publisher("pub1", flagController);
+        User user = new User("sub1", flagController);
 
         Thread publisher1 = new Thread(pub);
         Thread user1 = new Thread(user);
