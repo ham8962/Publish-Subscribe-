cd41f1fcd732fdeb34fa757c84160b05d07f14a8
ham8962 <zzing199842@gmial.com>
2023-07-17 20:17:31 +0900

    Publisher Class

--- src/main/java/org/example/Publisher.java
+++ src/main/java/org/example/Publisher.java
@@ -2,42 +2,54 @@ package org.example;
 
 import java.util.*;
 
-public class Publisher extends Thread {
+public class Publisher implements Runnable {
     private static Broker broker;
     private String name;
     private int input;
-    private boolean flag;
+    //private volatile boolean flag;
+    private final FlagSettable flagSettable;
 
-    public Publisher(String name){
+    public Publisher(String name, FlagSettable flagSettable) {
         this.name = name;
-        this.flag = false;
+        this.flagSettable = flagSettable;
+        this.broker = Broker.getInstance();
     }
 
+    /*
     public void publishCounter() {
         Scanner sc = new Scanner(System.in);
         input = sc.nextInt();
         broker = Broker.getInstance();
         if (input == 0) {
             flag = true;
-        }else{
+        } else {
             String message = "I'm a " + name;
-            for(int i = 0; i < input; i++){
+            for (int i = 0; i < input; i++) {
                 System.out.println("message is");
                 broker.publish(message);
             }
         }
     }
-
-
-    public boolean flagSignal() {
-        return flag;
-    }
-
+    */
 
     @Override
     public void run() {
-        while (!flag) {
-            publishCounter();
+        Scanner inputScanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter the input");
+            input = inputScanner.nextInt();
+            if (input == 0) {
+                System.out.println("Stop the System");
+                flagSettable.setFlag(true);
+                break;
+            } else {
+                String message = "It was published by " + name;
+                for (int i = 0; i < input; i++) {
+                    System.out.println(message);
+                    broker.publish(message);
+                }
+            }
+
         }
     }
 }
